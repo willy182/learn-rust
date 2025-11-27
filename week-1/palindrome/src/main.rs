3ee0723c2cd4b3b0a0969d8fe5753b0070c1cc66
Willy Kristianto <willykristianto89@gmail.com>
2025-11-28 00:47:54 +0700

    enhance fn palindrome

--- week-1/palindrome/src/main.rs
+++ week-1/palindrome/src/main.rs
@@ -1,39 +1,43 @@
-fn expand_from_center(chars: &[char], left: isize, right: isize) -> usize {
-    let mut l = left;
-    let mut r = right;
-    let len = chars.len() as isize;
+fn longest_palindrome_ascii_ci(s: &str) -> &str {
+    let b = s.as_bytes();
+    let n = b.len();
+    if n == 0 { return ""; }
 
-    while l >= 0 && r < len && chars[l as usize] == chars[r as usize] {
-        l -= 1;
-        r += 1;
-    }
+    let mut left = 0;
+    let mut right = 1;
 
-    (r - l - 1) as usize // panjang palindrome
-}
+    let expand = |b: &[u8], mut l: isize, mut r: isize| {
+        while l >= 0 && (r as usize) < b.len() {
+            let lc = b[l as usize].to_ascii_lowercase();
+            let rc = b[r as usize].to_ascii_lowercase();
 
-fn longest_palindrome(s: &str) -> String {
-    let chars: Vec<char> = s.chars().collect();
-    let n = chars.len();
-    if n == 0 { return "".to_string(); }
+            if lc != rc {
+                break;
+            }
 
-    let mut start = 0usize;
-    let mut end = 0usize;
+            l -= 1;
+            r += 1;
+        }
+
+        ((l + 1) as usize, (r - 1) as usize + 1)
+    };
 
     for i in 0..n {
-        let len1 = expand_from_center(&chars, i as isize, i as isize);       // odd
-        let len2 = expand_from_center(&chars, i as isize, i as isize + 1);   // even
-        let length = len1.max(len2);
-
-        if length > end - start {
-            start = i - (length - 1) / 2;
-            end   = i + length / 2;
+        for &(l, r) in &[
+            expand(b, i as isize, i as isize),
+            expand(b, i as isize, i as isize + 1),
+        ] {
+            if r - l > right - left {
+                left = l;
+                right = r;
+            }
         }
     }
 
-    chars[start..=end].iter().collect()
+    &s[left..right]
 }
 
 fn main() {
-    println!("{}", longest_palindrome("babad"));                         // bab atau aba
-    println!("{}", longest_palindrome("A man, a plan, a canal: Panama")); // amanakanama
+    println!("{}", longest_palindrome_ascii_ci("mantan teman duet maya estianti adalah pinkan mambo"));
+    println!("{}", longest_palindrome_ascii_ci("Level Ousmane Dembele sekarang ini sudah setara dengan Rodri"));
 }
